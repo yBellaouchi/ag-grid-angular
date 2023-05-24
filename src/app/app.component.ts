d4ff682a6d5d001a9370a160962348cdc1d92f78
younes bellaouchi <younessbellaouchi@gmail.com>
2023-05-24 16:34:26 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridOptions, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  // Each Column Definition results in one Column.
+ public columnDefs: ColDef[] = [
+  { field: 'make'},
+  { field: 'model'},
+  { field: 'price' }
+];
+
+ // DefaultColDef sets props common to all Columns
+ public defaultColDef: ColDef = {
+  sortable: true,
+  filter: true,
+  resizable: true,
+
+};
+
+ // Data that gets displayed in the grid
+ public rowData$!: Observable<any[]>;
+
+ @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+
+ constructor(private http: HttpClient) {}
+
+ // Example load data from server
+ onGridReady(params: GridReadyEvent) {
+  this.rowData$ = this.http
+    .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+    console.log('data', this.rowData$)
+}
+
+ // Example of consuming Grid Event
+ onCellClicked( e: CellClickedEvent): void {
+  console.log('cellClicked', e.data);
+}
+
+ // Example using Grid's API
+ clearSelection(): void {
+  this.agGrid.api.deselectAll();
+}
 }
